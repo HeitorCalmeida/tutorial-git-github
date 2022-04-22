7f7971e78f15a74050bb184a7f9885bdd10c6349
HeitorCalmeida <heitorcalmeida@gmail.com>
2022-04-22 18:41:49 -0300

    Criar novos dois arquivos java

---
+++ src/br/com/serratec/dominio/teste.java
@@ -0,0 +1,10 @@
+package br.com.serratec.dominio;
+
+public class teste {
+
+	public static void main(String[] args) {
+		System.out.println("Vou imprimir essa linha");
+		System.out.println("Linha dois para teste");
+	}
+
+}
